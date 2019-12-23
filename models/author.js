8b0e3735639ba907bada7b1a0b1233db0806fb78
CodinMaster <latheyapoorv@gmail.com>
2019-12-23 20:48:49 +0530

    author view route

--- models/author.js
+++ models/author.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
   }
+})
+
+//pre runs callback before excuting here: 'remove' command,
+authorSchema.pre('remove', function(next) {
+  Book.find({author: this.id}, (err, books) => {
+    if(err) {
+      next(err)
+    } else if(books.length > 0) {
+      next(new Error('This author still has books'))
+    } else {
+      next()
+    }
+  })
 })
 
 module.exports = mongoose.model('Author', authorSchema)
--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 //All Authors
 router.get('/', async (req, res) => {
@@ -42,8 +43,17 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', (req, res) => {
-  res.send()
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id}).limit(6)
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
 router.get('/:id/edit', async (req, res) => {
